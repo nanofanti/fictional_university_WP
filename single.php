daa7180b8f35cb716bd85a0a0f52ea7e669bcf34
Marco Fanti <fanti.marco8@gmail.com>
2024-08-13 15:48:41 +0200

    events, programs and archives

--- single.php
+++ single.php
@@ -19,7 +19,7 @@ the_post(  );?>
     <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
           <a class="metabox__blog-home-link" href="<?php echo site_url("/blog");?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home</a> <span class="metabox__main">
-          Posted by <?php the_author_posts_link();?> on <?php the_time("n.j.y"); ?> in <?php echo get_the_category_list(", ");?>
+          Posted by <?php the_author_posts_link();?> on <?php the_time("j.n.y"); ?> in <?php echo get_the_category_list(", ");?>
           </span>
         </p>
       </div>
